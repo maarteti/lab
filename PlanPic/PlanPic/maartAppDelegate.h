759a9a81bce0cc3cfb42e1804421afcf85a15368
jinwoo, choi <maarteti@gmail.com>
2011-11-24 00:21:07 +0900

    first lab project

---
+++ PlanPic/PlanPic/maartAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  maartAppDelegate.h
+//  PlanPic
+//
+//  Created by maart on 11. 11. 24..
+//  Copyright (c) 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface maartAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PlanPic/PlanPic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlanPic
+//
+//  Created by maart on 11. 11. 24..
+//  Copyright (c) 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "maartAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([maartAppDelegate class]));
+    }
+}
